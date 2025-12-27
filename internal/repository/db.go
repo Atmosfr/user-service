50d88f3ddbaaec9cbd527b0d93422c158a1bb9b3
Aleksei Gerasev <1999bag@gmail.com>
2025-12-27 15:38:14 +0300

    chore: rename

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewPool(ctx context.Context, dsn string) (*sql.DB, error) {
+func NewDB(ctx context.Context, dsn string) (*sql.DB, error) {
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		return nil, err
