c6f84fc0c7b011ea48449d378dcc7afeebd4113e
Aleksei Gerasev <1999bag@gmail.com>
2025-12-27 19:16:40 +0300

    feat: implement FindByEmail & FindByID

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -5,8 +5,8 @@ import (
 	"database/sql"
 	"errors"
 
-	"github.com/jackc/pgerrcode"
 	"github.com/Atmosfr/user-service/internal/models"
+	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
@@ -21,8 +21,8 @@ type userRepository struct {
 }
 
 func (r *userRepository) Create(ctx context.Context, user *models.User) error {
-	sql := `INSERT INTO users (email, password_hash, username) VALUES ($1, $2, $3) RETURNING id`
-	err := r.db.QueryRowContext(ctx, sql, user.Email, user.PasswordHash, user.Username).Scan(&user.ID)
+	query := `INSERT INTO users (email, password_hash, username) VALUES ($1, $2, $3) RETURNING id`
+	err := r.db.QueryRowContext(ctx, query, user.Email, user.PasswordHash, user.Username).Scan(&user.ID)
 	if err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
@@ -33,8 +33,34 @@ func (r *userRepository) Create(ctx context.Context, user *models.User) error {
 
 	return nil
 }
-func (r *userRepository) FindByEmail(ctx context.Context, email string) (*models.User, error) {}
-func (r *userRepository) FindByID(ctx context.Context, id int64) (*models.User, error)        {}
+
+func (r *userRepository) FindByEmail(ctx context.Context, email string) (*models.User, error) {
+	query := `SELECT id, email, password_hash, username, created_at, updated_at, is_active, role
+          FROM users WHERE email = $1`
+	user := &models.User{}
+	err := r.db.QueryRowContext(ctx, query, email).Scan(&user.ID, &user.Email, &user.PasswordHash, &user.Username, &user.CreatedAt, &user.UpdatedAt, &user.IsActive, &user.Role)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrUserNotFound
+		}
+		return nil, err
+	}
+	return user, nil
+}
+
+func (r *userRepository) FindByID(ctx context.Context, id int64) (*models.User, error) {
+	query := `SELECT id, email, password_hash, username, created_at, updated_at, is_active, role
+		  FROM users WHERE id = $1`
+	user := &models.User{}
+	err := r.db.QueryRowContext(ctx, query, id).Scan(&user.ID, &user.Email, &user.PasswordHash, &user.Username, &user.CreatedAt, &user.UpdatedAt, &user.IsActive, &user.Role)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrUserNotFound
+		}
+		return nil, err
+	}
+	return user, nil
+}
 
 func NewUserRepository(db *sql.DB) UserRepository {
 	return &userRepository{db: db}
