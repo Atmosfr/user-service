784b8730edd5d39c7ff8e7927ecd0e4730f6bd8f
Aleksei Gerasev <1999bag@gmail.com>
2026-01-13 14:00:33 +0300

    feat: ErrInvalidCredentials error

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrEmailAlreadyExists = errors.New("email already exists")
 	ErrUserNotFound       = errors.New("user not found")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
