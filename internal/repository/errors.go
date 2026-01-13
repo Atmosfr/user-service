3a9d0bb9138698ec7d60258a358c50532020d8e8
Aleksei Gerasev <1999bag@gmail.com>
2026-01-13 14:51:43 +0300

    feat: ErrInvalidPassword error

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrEmailAlreadyExists = errors.New("email already exists")
 	ErrUserNotFound       = errors.New("user not found")
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidPassword	= errors.New("invalid password")
 )
