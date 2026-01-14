543d07e4b1099c2b7167614ef274f4fa94e17eca
Aleksei Gerasev <1999bag@gmail.com>
2026-01-14 23:24:31 +0300

    feat: jwt module

---
+++ internal/auth/jwt.go
@@ -0,0 +1,59 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Atmosfr/user-service/internal/models"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserID int64  `json:"user_id"`
+	Role   string `json:"user_role"`
+	jwt.RegisteredClaims
+}
+
+var jwtSecret = []byte("secret")
+
+func GenerateToken(user *models.User) (string, error) {
+	claims := Claims{
+		UserID: user.ID,
+		Role:   user.Role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtSecret)
+	return tokenString, err
+}
+
+func ValidateToken(tokenString string) (*models.User, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	user := &models.User{
+		ID:   claims.UserID,
+		Role: claims.Role,
+	}
+
+	return user, nil
+}
