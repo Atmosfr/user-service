4dfc5336c78c64992ade489fc079c064b9d38dd6
Aleksei Gerasev <1999bag@gmail.com>
2026-01-30 15:58:53 +0300

    test: TestUserService_Login

--- internal/service/user_test.go
+++ internal/service/user_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Atmosfr/user-service/internal/validation"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type mockUserRepo struct {
@@ -166,6 +167,152 @@ func TestUserService_Register(t *testing.T) {
 			require.Equal(t, tt.username, user.Username)
 
 			repo.AssertNumberOfCalls(t, "Create", 1)
+			repo.AssertExpectations(t)
+		})
+	}
+}
+
+func TestUserService_Login(t *testing.T) {
+	ctx := context.Background()
+	tests := []struct {
+		name       string
+		username   string
+		role       string
+		email      string
+		password   string
+		setupMock  func(repo *mockUserRepo)
+		wantErr    error
+		wantUserID int64
+	}{
+		{
+			name:     "valid login",
+			username: "existing",
+			role:     "user",
+			email:    "existing@example.com",
+			password: "StrongPass!12",
+			setupMock: func(repo *mockUserRepo) {
+				hashed, _ := bcrypt.GenerateFromPassword([]byte("StrongPass!12"), bcrypt.DefaultCost)
+				user := &models.User{
+					ID:           1,
+					Email:        "existing@example.com",
+					PasswordHash: string(hashed),
+					Username:     "existing",
+					Role:         "user",
+				}
+				repo.On("FindByEmail", mock.Anything, user.Email).Return(user, nil)
+			},
+			wantErr:    nil,
+			wantUserID: 1,
+		},
+		{
+			name:     "invalid password",
+			email:    "existing@example.com",
+			password: "StrongPass!122",
+			setupMock: func(repo *mockUserRepo) {
+				hashed, _ := bcrypt.GenerateFromPassword([]byte("StrongPass!12"), bcrypt.DefaultCost)
+				user := &models.User{
+					ID:           1,
+					Email:        "existing@example.com",
+					PasswordHash: string(hashed),
+					Username:     "existing",
+					Role:         "user",
+				}
+				repo.On("FindByEmail", mock.Anything, user.Email).Return(user, nil)
+			},
+			wantErr: repository.ErrInvalidPassword,
+		},
+		{
+			name:     "user not found",
+			email:    "ex@example.com",
+			password: "StrongPass!12",
+			setupMock: func(repo *mockUserRepo) {
+				repo.On("FindByEmail", mock.Anything, "ex@example.com").Return((*models.User)(nil), repository.ErrUserNotFound)
+			},
+			wantErr: repository.ErrUserNotFound,
+		},
+		{
+			name:     "invalid email format",
+			email:    "invalid",
+			password: "StrongPass!12",
+			setupMock: func(repo *mockUserRepo) {
+				// No repository calls expected
+			},
+			wantErr: validation.ErrInvalidEmail,
+		},
+		{
+			name:     "empty email",
+			email:    "",
+			password: "StrongPass!12",
+			setupMock: func(repo *mockUserRepo) {
+				// No repository calls expected
+			},
+			wantErr: validation.ErrInvalidEmail,
+		},
+		{
+			name:     "empty password",
+			email:    "ex@example.com",
+			password: "",
+			setupMock: func(repo *mockUserRepo) {
+				// No repository calls expected
+			},
+			wantErr: validation.ErrInvalidCredentials,
+		},
+		{
+			name:     "short password",
+			email:    "ex@example.com",
+			password: "g$12",
+			setupMock: func(repo *mockUserRepo) {
+				// No repository calls expected
+			},
+			wantErr: validation.ErrPasswordTooShort,
+		},
+		{
+			name:     "empty jwt secret",
+			email:    "existing@example.com",
+			password: "StrongPass!12",
+			setupMock: func(repo *mockUserRepo) {
+				auth.JwtSecret = []byte("")
+				hashed, _ := bcrypt.GenerateFromPassword([]byte("StrongPass!12"), bcrypt.DefaultCost)
+				user := &models.User{
+					ID:           1,
+					Email:        "existing@example.com",
+					PasswordHash: string(hashed),
+					Username:     "existing",
+					Role:         "user",
+				}
+				repo.On("FindByEmail", mock.Anything, user.Email).Return(user, nil)
+			},
+			wantErr: auth.ErrEmptyJwtSecret,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			repo := new(mockUserRepo)
+			svc := NewUserService(repo)
+			auth.JwtSecret = []byte("secret")
+
+			tt.setupMock(repo)
+
+			logResponse, err := svc.Login(ctx, tt.email, tt.password)
+
+			if tt.wantErr != nil {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.wantErr.Error())
+				return
+			}
+
+			require.NoError(t, err)
+			require.NotNil(t, logResponse)
+			require.NotEmpty(t, logResponse.Token)
+
+			user := logResponse.User
+			require.Empty(t, user.PasswordHash)
+			require.Equal(t, tt.wantUserID, user.ID)
+			require.Equal(t, tt.username, user.Username)
+			require.Equal(t, tt.role, user.Role)
+			require.Equal(t, tt.email, user.Email)
+
+			repo.AssertNumberOfCalls(t, "FindByEmail", 1)
 			repo.AssertExpectations(t)
 		})
 	}
